efa0a7cac005b86bc2fff59e325170c8b53904bd
NVBao Dev <nvbluutru@gmail.com>
2023-08-09 09:27:56 +0700

    first commit

---
+++ src/main/java/vn/plantpal/mobile_backend/MobileBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.plantpal.mobile_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/plantpal/mobile_backend/MobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.plantpal.mobile_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
