4f86fe9f428acf09a3809e47c77e8f18bc29d3ed
NVBao Dev <nvbluutru@gmail.com>
2023-08-10 11:46:57 +0700

    update open ai

--- src/main/java/vn/plantpal/mobile_backend/configs/swagger/OpenApiConfig.java
+++ src/main/java/vn/plantpal/mobile_backend/configs/swagger/OpenApiConfig.java
@@ -5,15 +5,20 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+    @Value("${server-url}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url(serverUrl))
                 .components(new Components()
                         .addSecuritySchemes("JWTAuth",
                                 new SecurityScheme()
